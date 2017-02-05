a13d8b2ee2d860d58d54c30ad5855a8aad4febec
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-02-04 16:46:10 -0800

    added migrate fix

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204223008) do
+ActiveRecord::Schema.define(version: 20170205003649) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
