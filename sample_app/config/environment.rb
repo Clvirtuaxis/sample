e90ddc2b74acef9b7f2e09f0d0337a4f52397f3c
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-02-20 22:44:53 -0800

    5.3 mailer v4

--- sample_app/config/environment.rb
+++ sample_app/config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['carolac'],
-  :password => ENV['indi7788'],
+  :user_name => ENV['apikey'],
+  :password => ENV['VirtuaxisKeySMTP'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
