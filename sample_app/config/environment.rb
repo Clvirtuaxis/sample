478361b9f51d6991197147b12f69ab1809f9d5f9
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-02-20 23:28:34 -0800

    5.3 mailer v8

--- sample_app/config/environment.rb
+++ sample_app/config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['apikey'],
-  :password => ENV['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'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
