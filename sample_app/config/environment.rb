15922f5bc535d104d8ae358afdaa572addb3f691
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-02-20 22:51:06 -0800

    5.3 mailer v5

--- sample_app/config/environment.rb
+++ sample_app/config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :port => '587',
   :authentication => :plain,
   :user_name => ENV['apikey'],
-  :password => ENV['VirtuaxisKeySMTP'],
+  :password => ENV['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'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
