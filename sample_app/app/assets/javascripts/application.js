71d6ae26d608916468e6e300e233842340935f30
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-02-24 20:38:03 -0800

    5.5 authorization

--- sample_app/app/assets/javascripts/application.js
+++ sample_app/app/assets/javascripts/application.js
@@ -24,12 +24,16 @@ $(document).click(function (event) {
   }
 });
 
-// $( document ).ready(function() {
-//   var heights = $(".item").map(function() {
-//     return $(this).height();
-//   }).get(),
+$(document).ready(function () {
+	window.setTimeout(function() {
+	  $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
+	      $(this).remove(); 
+	  });
+	}, 2000);
+});
 
-//   maxHeight = Math.max.apply(null, heights);
 
-//   $(".item").height(maxHeight);
-// });
+// $(document).ready(function(){
+//     alert("jkgkjb,j,");
+//   });
+
