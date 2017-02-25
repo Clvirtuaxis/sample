c39f4807cc53825cc99687c5c302b5b1a0ced189
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-02-25 11:03:58 -0800

    5.5 fix alerts

--- sample_app/app/assets/javascripts/application.js
+++ sample_app/app/assets/javascripts/application.js
@@ -26,7 +26,7 @@ $(document).click(function (event) {
 
 $(document).ready(function () {
 	window.setTimeout(function() {
-	  $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
+	  $(".alert").fadeTo(1300, 0).slideUp(1000, function(){
 	      $(this).remove(); 
 	  });
 	}, 2000);
