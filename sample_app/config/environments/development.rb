a81f209c4b81aae26697bfdc22141fcf98da1041
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-03-12 18:55:30 -0700

    fixes

--- sample_app/config/environments/development.rb
+++ sample_app/config/environments/development.rb
@@ -27,12 +27,14 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   # Dynamic links inside emails from your dev environment generate the right URLs.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
