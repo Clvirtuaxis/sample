b1e2e25237c25221de85c6ea9c03755c21e5e044
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-03-17 12:06:49 -0700

    6.3 test controller

--- sample_app/spec/controllers/users_controller_spec.rb
+++ sample_app/spec/controllers/users_controller_spec.rb
@@ -3,28 +3,34 @@ require 'rails_helper'
 describe UsersController, :type => :controller do
   
   let(:user) { User.create!(email: 'something@something.com', password: 'test12345') }
-  
-  
+  let(:user2) { User.create!(email: 'test@test.com', password: 'testtest') }
   
   describe 'GET #show' do
+
     context 'User is logged in' do
       before do
         sign_in user
       end
+
       it 'loads correct user details' do
         get :show, id: user.id
         expect(response).to be_success
         expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq user
       end
+
+      it 'User can not access user1 info' do
+        get :show, id: user2.id
+        expect(response).to have_http_status(200)
+        expect(response).to be_success
+      end
     end
     
     context 'User is not logged in' do
       it 'redirects to login' do
         get :show, id: user.id
-        expect(response).to redirect_to("/login")
+        expect(response).to redirect_to('/login')
       end
     end
-        
   end
 end
