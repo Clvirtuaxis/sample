426cbfe82614638331d82d1f83b842411985dfcd
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-03-20 18:52:48 -0700

    6.6 create payments

---
+++ sample_app/app/controllers/payments_controller.rb
@@ -0,0 +1,32 @@
+class PaymentsController < ApplicationController
+  
+  def create
+    token = params[:stripeToken]
+		@product = Product.find(params[:product_id])
+    @user = current_user
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => @product.price_in_cents, # amount in cents, again
+        :currency => "cad",
+        :source => token,
+        :description => params[:stripeEmail]
+        )
+
+      	if charge.paid
+      		Order.create(
+	      		product_id: "@product_id",
+	      		user_id: "@user_id", 
+	      		total: "@total"
+      			)
+  			end
+
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+    	err = body[:error]
+    	flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+    redirect_to product_path(@product), notice: "Thank you for purchasing at Virtuaxis!"
+  end
+end
