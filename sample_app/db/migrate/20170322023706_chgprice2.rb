88caba9325eff29986707df80e7c767640a1004b
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-03-21 19:47:41 -0700

    fix migrate3

--- sample_app/db/migrate/20170322023706_chgprice2.rb
+++ sample_app/db/migrate/20170322023706_chgprice2.rb
@@ -1,5 +1,6 @@
 class Chgprice2 < ActiveRecord::Migration[5.0]
   def change
-      change_column_null :products, :price, :decimal, precision: 5, scale: 2
+      #change_column_null :products, :price, :decimal, precision: 5, scale: 2
+      add_column :products, :price, :decimal
   end
 end
