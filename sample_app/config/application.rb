6e5e97e5f7904ccc19e077861c028497b3f28d31
Caroline Lachance <caroline.lachance@virtuaxis.com>
2017-04-10 13:31:16 -0400

    6.12 install Redis

--- sample_app/config/application.rb
+++ sample_app/config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,5 +11,7 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+
   end
 end
